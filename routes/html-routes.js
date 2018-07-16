a93fec6b9facbbb94b40245b845c70b9ffb3f9ac
atkinsta <atkinstaa@gmail.com>
2018-07-16 19:01:29 -0400

    Adding views to heroku deploy for testing

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,8 +4,7 @@ let Burger = db.Burger;
 module.exports = app => {
     app.get("/", (req, res) => {
         Burger.findAll({}).then(data => {
-            res.json(data);
-            // res.render("index", { burgers: data })
+            res.render("index", { burgers: data })
         })
     })
 }
