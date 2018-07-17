df679b68bb31900162883415a55eb1f41fd93788
atkinsta <atkinstaa@gmail.com>
2018-07-17 18:13:17 -0400

    Added event handlers, fixed handlebars, some style

--- public/assets/javascript/logic.js
+++ public/assets/javascript/logic.js
@@ -1,3 +1,43 @@
 $(document).ready(function () {
-    console.log("working");
-})
+
+    $(document).on("keydown", "#newburger", function(event) { // Alows the user to press enter instead of clicking the button.
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            addBurger();
+        }
+    })
+
+    $(document).on("click", "#addburger", function() {
+        addBurger();
+    });
+
+    $(document).on("click", ".eat", function() {
+        $.ajax({
+            url: "api/burgers/" + $(this).attr("data-id"),
+            method: "PUT",
+            data: {eaten: true}
+        }).then(function () {
+            location.href = "/";
+        });
+    });
+
+    $(document).on("click", ".delete", function() {
+        $.ajax({
+            url: "api/burgers/" + $(this).attr("data-id"),
+            method: "DELETE"
+        }).then(function() {
+            location.href = "/"
+        });
+    });
+});
+
+function addBurger() {
+    let newBurger = {
+        name: $("#newburger").val().trim(),
+        eaten: false
+    };
+    $.post("/api/burgers", newBurger, function() {
+        $("#newburger").val("");
+        location.href = "/";
+    });
+}
